c0f7197efc687b170ee538018ddac49313096197
uwei <forwarto9@gmail.com>
2020-02-15 12:37:14 +0800

    update

---
+++ HelloWorld/HelloWorld/Student.c
@@ -0,0 +1,24 @@
+//
+//  Student.c
+//  HelloWorld
+//
+//  Created by uwei on 2020/2/12.
+//  Copyright © 2020 yuanyouguo. All rights reserved.
+//
+
+#include "Student.h"
+
+#ifndef NULL
+#define NULL (0)
+#endif
+int ii = 99;
+
+char *  name () {
+    int i = 0;
+    return "hello";
+}
+void printName() {
+    const char *string = "youguo";
+    char *name = NULL;
+    printf("%d", name[10]);
+}
---
+++ HelloWorld/HelloWorld/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by uwei on 2020/2/12.
+//  Copyright © 2020 yuanyouguo. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include "Student.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int name[10]={0};
+    printf("%d",name[11]);
+    
+    
+    
+    return 0;
+}
+
+
